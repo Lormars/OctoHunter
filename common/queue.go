d1ada9e307d89c65e2d790ac8af518b087ad71d1
lormars <1780228262@qq.com>
2024-07-16 17:08:19 +0800

    refine

--- common/queue.go
+++ common/queue.go
@@ -166,6 +166,6 @@ func dispatchBroker() {
 			broker.ch <- true
 			close(broker.ch)
 		}
-		time.Sleep(500 * time.Millisecond)
+		time.Sleep(time.Duration(1/(bq.Len()+1)) * 500 * time.Millisecond)
 	}
 }
