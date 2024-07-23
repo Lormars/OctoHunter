e0000b5b60f8dcfa8fec95946a68b4ab1e17c963
lormars <1780228262@qq.com>
2024-07-23 17:35:25 +0800

    refine

--- common/clients/h0.go
+++ common/clients/h0.go
@@ -26,7 +26,7 @@ func (t *H0Transport) RoundTrip(req *http.Request) (*http.Response, error) {
 			//logger.Warnf("HTTP/2 request succeeded for %s\n", req.URL.String())
 			return resp, nil
 		}
-		logger.Warnf("HTTP/2 request failed: %v, falling back to HTTP/1.1 for %s\n", err, req.URL.String())
+		// logger.Warnf("HTTP/2 request failed: %v, falling back to HTTP/1.1 for %s\n", err, req.URL.String())
 	}
 	return t.h1Transport.RoundTrip(req)
 }
