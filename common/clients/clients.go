d7abc554fa4e2f876e6ed90b96c3941d0f34fe89
lormars <1780228262@qq.com>
2024-07-30 18:44:09 +0800

    refine

--- common/clients/clients.go
+++ common/clients/clients.go
@@ -28,7 +28,7 @@ var (
 	cleanupInterval        = 60 * time.Second
 	maxIdleTime            = 120 * time.Second
 	totalData        int64 = 0
-	concurrentReq          = make(chan struct{}, 160)
+	concurrentReq          = make(chan struct{}, 800)
 	mu               sync.Mutex
 	resStats         = make(map[string][]*responseStats)
 	allRequestsCount = 0
